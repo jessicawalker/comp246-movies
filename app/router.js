e608067ef25c9f11e150edd9953b646a78d09e93
Jessica Walker <jessicacrotty@gmail.com>
2020-12-03 00:26:12 -0500

    Adds main HTML and JS for AngularJS page.

--- app/router.js
+++ app/router.js
@@ -13,6 +13,10 @@ var router = function(app) {
     app.get("/browse-movies", function(req, res) {
         res.status(200).sendFile(clientPath + "/browse-movies.html");
     });
+
+    app.get("/view-movies", function(req, res) {
+        res.status(200).sendFile(clientPath + "/view-movies.html");
+    });
 };
 
 module.exports = router;
---
+++ client/js/view-movies.js
@@ -0,0 +1,38 @@
+var movies = [];
+var activeMovie = 0;
+
+var app = angular.module("viewMoviesApp", []);
+
+app.controller("viewMoviesController", function($scope, $http) {
+    $scope.movies = [];
+
+    $scope.get_movies = function() {
+        $http({
+            method: "GET",
+            url: "http://localhost:5500/read-records"
+        }).then(function(response) {
+            if (response.data.msg === "SUCCESS") {
+                movies = response.data.movies;
+                $scope.obj = movies[activeMovie];
+                $scope.showHide();
+            } else {
+                $scope.addResults = response.data.msg;
+            }
+        }, function(response) {
+            console.log(response);
+        });
+    };
+
+    $scope.get_movies();
+
+    $scope.changeMovie = function(direction) {
+        activeMovie += direction;
+        $scope.obj = movies[activeMovie];
+        $scope.showHide();
+    }
+
+    $scope.showHide = function() {
+        $scope.hidePrev = (activeMovie === 0) ? true : false;
+        $scope.hideNext = (activeMovie === movies.length - 1) ? true : false;
+    };
+});
