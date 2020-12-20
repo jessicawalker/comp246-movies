0ce51cd82c1bd83b1be095afd3c82ec69cae55a6
Jessica Walker <jessicacrotty@gmail.com>
2020-12-20 02:18:37 -0500

    Update services db.

--- app/services.js
+++ app/services.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 
-const dbURL = process.env.DB_URI || "mongodb+srv://jessicaawalker:Len4Macca0627@cluster0.aocuq.mongodb.net/movies?retryWrites=true&w=majority" || "mongodb://localhost";
+const dbURL = process.env.DB_URI || "mongodb+srv://jessicaawalker:Len4Macca0627@cluster0.aocuq.mongodb.net/movies?retryWrites=true&w=majority";
 
 
 // Service listeners
